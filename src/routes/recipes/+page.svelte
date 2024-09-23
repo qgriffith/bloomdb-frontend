2857a843d61fd8535d42c1a4e12674d793fbc9a9
qgriffith <qgriffith@gmail.com>
2024-09-23 16:51:54 -0400

    sync

---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { onMount } from "svelte";
+
+	const api_endpoint = "https://bloomdb-production.up.railway.app/api/recipes";
+	let recipes = [];
+
+	onMount(async () => {
+		console.log("onMount");
+		try {
+			const response = await fetch(api_endpoint);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			recipes = await response.json();
+			console.log(recipes);
+		} catch (error) {
+			console.error("Failed to fetch recipes:", error);
+		}
+	});
+</script>
+<main>
+	<p class="h2">Recipes</p>
+	<dl class="list-dl">
+		{#each recipes as recipe}
+			<div>
+				<span class="badge bg-orange-800">☕</span>
+				<span class="flex-auto">
+					<dt class="font-bold"><a class="anchor" href="recipe/{recipe.id}">{recipe.title}</a></dt>
+					<dd class="text-sm opacity-50">{recipe.roaster}</dd>
+				</span>
+			</div>
+		{/each}
+	</dl>
+</main>
+
