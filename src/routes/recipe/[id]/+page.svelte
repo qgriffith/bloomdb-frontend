312efd768106cf4fe48c37497ed0829faec45b98
qgriffith <qgriffith@gmail.com>
2024-09-23 16:39:53 -0400

    Add recipe details page and cleanup dependencies
    Implemented a new Svelte page to fetch and display recipe details based on recipe ID. Cleaned up the package-lock.json by removing unnecessary development dependencies.

---
+++ src/routes/recipe/[id]/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from "svelte";
+	export let data;
+	const api_endpoint = `https://bloomdb-production.up.railway.app/api/recipe/${data.id}`;
+	let recipe = [];
+
+	onMount(async () => {
+		console.log("onMount");
+		try {
+			const response = await fetch(api_endpoint);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			recipe = await response.json();
+			console.log(recipe);
+		} catch (error) {
+			console.error("Failed to fetch recipes:", error);
+		}
+	});
+</script>
+<main>
+	<div class="card variant-filled">
+		<header class="card-header">{recipe.title}</header>
+		<section class="p-4">
+			<ul class="list">
+				<li>Link: <a class="p-2 bg-secondary-hover-token anchor" href="{recipe.link}">Recipe</a></li>
+				<li>Temp: <span class="p-2">{recipe.temp}</span></li>
+				<li>Brewer: <span class="p-2">Omni</span></li>
+				<li>Roaster: <span class="p-2">{recipe.roaster}</span></li>
+				<li>Roast Level: <span class="p-2">Light-Medium</span></li>
+				<li>Creator: <span class="p-2">{recipe.creator}</span></li>
+				<li>Buy: <a class="p-2 bg-secondary-hover-token anchor" href="{recipe.shop_link}">Shop</a></li>
+			</ul>
+		</section>
+	</div>
+</main>
