b243a5400bcb3be2f8c636193986afb2ae32a283
qgriffith <qgriffith@gmail.com>
2024-09-30 12:36:22 -0400

    Refactor external links and media query styles
    Moved external links from `+layout.svelte` to `Navigation.svelte`, ensuring a cleaner and more modular structure. Adjusted the `max-width: 640px` media query to refine smaller screen styles, specifically the font-size for `header-title`.

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { House, Coffee, Plus } from 'lucide-svelte';
-	import { getDrawerStore } from "@skeletonlabs/skeleton";
+	import { House, Coffee, Plus, GithubIcon, MessageCircleMore, Link } from 'lucide-svelte';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
 	const drawerStore = getDrawerStore();
 
@@ -15,4 +15,13 @@
 		<li><a href="/recipes" on:click={drawerClose}><span class="p-1"><Coffee /></span>Recipes</a></li>
 		<li><a href="/recipe/add" on:click={drawerClose}><span class="p-1"><Plus /></span>New</a></li>
 	</ul>
+</nav>
+<hr class="!border-dashed" />
+<h6 class="h6">External Links</h6>
+<nav class="list-nav p-4">
+<ul>
+	<li><a href="http://github.com/qgriffith"  target="_blank" on:click={drawerClose}><span class="p-2"><GithubIcon /></span>Github</a></li>
+	<li><a href="https://xbloom.com"  target="_blank" on:click={drawerClose}><span class="p-1"><Link /></span>Xbloom</a></li>
+	<li><a href="https://discord.gg/DYG6Jxte9C" target="_blank" on:click={drawerClose}><span class="p-1"><MessageCircleMore /></span>Discord</a></li>
+</ul>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,10 +16,7 @@
 <style>
     /* Custom Media Query for sm screens */
     @media (max-width: 640px) {
-        .sidebar {
-            width: 100%;
-        }
-        .header-title {
+           .header-title {
             font-size: 1.5rem;
         }
     }
@@ -48,32 +45,6 @@
 					</button>
 					<strong class="header-title text-xl uppercase">Xbloomdb</strong>
 				</div>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="http://github.com/qgriffith"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Github
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://xbloom.com"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Xbloom
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/DYG6Jxte9C"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
