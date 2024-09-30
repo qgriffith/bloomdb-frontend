91d87c3dc80deac9134ce8c28ba36376d4f75523
qgriffith <qgriffith@gmail.com>
2024-09-30 10:56:52 -0400

    Fix formatting and add custom styles for responsiveness.
    Corrected inconsistent formatting and improved readability by aligning HTML elements. Added a media query for smaller screens to adjust the sidebar width and header title font size for better responsiveness.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar} from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import Navigation from '$lib/Navigation/Navigation.svelte';
 	import { initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	// Floating UI for Popups
@@ -8,17 +8,27 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	initializeStores();
-
 	const drawerStore = getDrawerStore();
 	function drawerOpen(): void {
 		drawerStore.open({});
 	}
 </script>
-
+<style>
+    /* Custom Media Query for sm screens */
+    @media (max-width: 640px) {
+        .sidebar {
+            width: 100%;
+        }
+        .header-title {
+            font-size: 1.5rem;
+        }
+    }
+    /* You can add more custom styles if needed */
+</style>
 <Drawer>
 	<h2 class="p-4">Navigation</h2>
 	<hr />
-<Navigation/>
+	<Navigation />
 </Drawer>
 <!-- App Shell -->
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
@@ -28,15 +38,15 @@
 			<svelte:fragment slot="lead">
 				<div class="flex items-center">
 					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-            <span>
-                <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-                    <rect width="100" height="20" />
-                    <rect y="30" width="100" height="20" />
-                    <rect y="60" width="100" height="20" />
-                </svg>
-            </span>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
 					</button>
-					<strong class="text-xl uppercase">Xbloomdb</strong>
+					<strong class="header-title text-xl uppercase">Xbloomdb</strong>
 				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
@@ -70,9 +80,7 @@
 	<!-- Page Route Content -->
 	<!-- Left Sidebar Slot -->
 	<svelte:fragment slot="sidebarLeft">
-	<Navigation />
-
+		<Navigation />
 	</svelte:fragment>
 	<slot />
 </AppShell>
-
