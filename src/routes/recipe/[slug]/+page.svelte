5cfc1b9be1b7dcaa7cf722fb7f78d594cc89041e
qgriffith <qgriffith@gmail.com>
2024-09-30 10:17:14 -0400

    Fetch and display brewer and roast details
    Added fetch calls for brewer and roast details based on recipe IDs. Updated the recipe display to include dynamic brewer type and roast level from the fetched data. Improved error handling for fetch operations.

--- src/routes/recipe/[slug]/+page.svelte
+++ src/routes/recipe/[slug]/+page.svelte
@@ -3,7 +3,10 @@
 	export let data;
 	import {PUBLIC_API_URL} from '$env/static/public';
 	const api_endpoint = `${PUBLIC_API_URL}/recipe/${data.slug}`;
+	let brewer = [];
+	let roast = [];
 	let recipe = [];
+
 
 	onMount(async () => {
 		console.log("onMount");
@@ -19,6 +22,30 @@
 		} catch (error) {
 			console.error("Failed to fetch recipes:", error);
 		}
+		const brew_endpoint = `${PUBLIC_API_URL}/brewer/${recipe.brewer_id}`;
+		const roast_endpoint = `${PUBLIC_API_URL}/roast/${recipe.roast_id}`;
+		try {
+			const response = await fetch(brew_endpoint);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			brewer = await response.json();
+		} catch (error) {
+			console.error("Failed to fetch brewers:", error);
+		}
+		try {
+			const response = await fetch(roast_endpoint);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			roast = await response.json();
+		} catch (error) {
+			console.error("Failed to fetch roasts:", error);
+		}
 	});
 </script>
 <main>
@@ -28,9 +55,9 @@
 			<ul class="list">
 				<li>Link: <a class="p-2 bg-secondary-hover-token anchor" href="{recipe.link}">Recipe</a></li>
 				<li>Temp: <span class="p-2">{recipe.temp}</span></li>
-				<li>Brewer: <span class="p-2">Omni</span></li>
+				<li>Brewer: <span class="p-2">{brewer.type}</span></li>
 				<li>Roaster: <span class="p-2">{recipe.roaster}</span></li>
-				<li>Roast Level: <span class="p-2">Light-Medium</span></li>
+				<li>Roast Level: <span class="p-2">{roast.level}</span></li>
 				<li>Creator: <span class="p-2">{recipe.creator}</span></li>
 				<li>Submitter:<span class="p-2">{recipe.oauth_user}</span></li>
 				<li>Buy: <a class="p-2 bg-secondary-hover-token anchor" href="{recipe.shop_link}">Shop</a></li>
